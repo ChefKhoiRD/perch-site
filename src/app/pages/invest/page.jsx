96165ecd7ab6ae0dc4793324c81c57bc788bc0d0
Chef <kvu6111@gmail.com>
2024-03-24 11:37:49 -0600

    started invest page

--- src/app/pages/invest/page.jsx
+++ src/app/pages/invest/page.jsx
@@ -1,7 +1,36 @@
 import NavBar from "../../../components/common/navBar";
 
+import "./styles/invest.css";
+
 export default function Invest() {
   return (
-    <NavBar active="invest" />
+    <div className="page-content">
+      <NavBar active="invest" />
+
+      <div className="invest-container">
+
+        {/* Header */}
+        <div className="invest-header">
+          <h1 className="">Invest in Perch</h1>
+        </div>
+
+        {/* Subheader */}
+        <div className="invest-subheader">
+          <h2 className="">The future of restaurant management</h2>
+        </div>
+
+        {/* Description */}
+        <div className="invest-description">
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic eveniet quia molestiae, nisi doloremque veritatis aliquam perferendis mollitia cupiditate inventore architecto, animi amet, porro expedita odio ex minima tempore perspiciatis?
+            </p>
+        </div>
+
+        {/* Invest button */}
+        <div className="invest-button">
+          <button className="">Invest in us</button>
+        </div>
+      </div>
+    </div>
   );
 }
